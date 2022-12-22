0036b653b25cd467448df810936ae94556233c8b
polliterru <polinasilnagina3313@gmail.com>
2022-12-22 13:14:20 +0200

    fix: navbar height after scroll

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
 const navbar = document.querySelector('.navbar')
+const logo = document.querySelector('.logo-svg use')
 
 window.addEventListener('scroll', function() {
   if (window.scrollY > 99) {
     navbar.classList.add('navbar-light')
+    logo.href.baseVal = 'img/sprite.svg#logo'
   } else {
     navbar.classList.remove('navbar-light')
+    logo.href.baseVal = 'img/sprite.svg#logo-light'
   }
 })
