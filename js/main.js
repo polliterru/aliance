ba69326a2370943eb00f883d657d0b6a28c774d3
polliterru <polinasilnagina3313@gmail.com>
2022-12-28 00:44:59 +0200

    add: mobile menu

--- js/main.js
+++ js/main.js
@@ -1,12 +1,33 @@
 const navbar = document.querySelector('.navbar')
 const logo = document.querySelector('.logo-svg use')
+const mobileMenuToggle = document.querySelector('.js-mobile-menu-toggle')
+const mobileMenu = document.querySelector('.mobile-menu')
+
+const addNavbarLight = () => {
+  navbar.classList.add('navbar-light')
+  logo.href.baseVal = 'img/sprite.svg#logo'
+}
+const removeNavbarLight = () => {
+  navbar.classList.remove('navbar-light')
+  logo.href.baseVal = 'img/sprite.svg#logo-light'
+}
 
 window.addEventListener('scroll', function() {
   if (window.scrollY > 99) {
-    navbar.classList.add('navbar-light')
-    logo.href.baseVal = 'img/sprite.svg#logo'
+    addNavbarLight()
   } else {
-    navbar.classList.remove('navbar-light')
-    logo.href.baseVal = 'img/sprite.svg#logo-light'
+    removeNavbarLight()
+  }
+})
+
+mobileMenuToggle.addEventListener('click', () => {
+  mobileMenuToggle.classList.toggle('active')
+  mobileMenu.classList.toggle('is-open')
+  document.documentElement.classList.toggle('no-scroll')
+  document.body.classList.toggle('no-scroll')
+  addNavbarLight()
+
+  if (!mobileMenu.classList.contains('is-open') && window.scrollY === 0) {
+    removeNavbarLight()
   }
 })
