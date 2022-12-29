00e75456efb34c727905325dd57f9dc897b18a6c
polliterru <polinasilnagina3313@gmail.com>
2022-12-29 21:10:55 +0200

    add: responsive home screen and slider

--- js/main.js
+++ js/main.js
@@ -3,6 +3,11 @@ const logo = document.querySelector('.logo-svg use')
 const mobileMenuToggle = document.querySelector('.js-mobile-menu-toggle')
 const mobileMenu = document.querySelector('.mobile-menu')
 
+console.log(navbar);
+console.log(logo);
+console.log(mobileMenuToggle);
+console.log(mobileMenu);
+
 const addNavbarLight = () => {
   navbar.classList.add('navbar-light')
   logo.href.baseVal = 'img/sprite.svg#logo'
@@ -26,4 +31,72 @@ mobileMenuToggle.addEventListener('click', () => {
   if (!mobileMenu.classList.contains('is-open') && window.scrollY === 0) {
     removeNavbarLight()
   }
+})
+
+const swiperFeatures = new Swiper('.swiper-features', {
+  speed: 400,
+  autoHeight: true,
+  loop: true,
+  navigation: {
+    nextEl: '.slider-button-next',
+    prevEl: '.slider-button-prev',
+  },
+  breakpoints: {
+    // when window width is >= 576px
+    0: {
+      slidesPerView: 1,
+    },
+    // when window width is >= 768px
+    768: {
+      slidesPerView: 3,
+    },
+    // when window width is >= 1024px
+    1024: {
+      slidesPerView: 4,
+    },
+    // when window width is >= 1200px
+    1200: {
+      slidesPerView: 5,
+    }
+  }
+})
+
+const swiperStep = new Swiper('.swiper-step', {
+  speed: 400,
+  autoHeight: true,
+  loop: true,
+  navigation: {
+    nextEl: '.slider-step-button-next',
+    prevEl: '.slider-step-button-prev',
+  },
+  breakpoints: {
+    // when window width is >= 576px
+    0: {
+      slidesPerView: 1,
+    },
+    // when window width is >= 768px
+    768: {
+      slidesPerView: 2,
+    },
+    // when window width is >= 1024px
+    1024: {
+      slidesPerView: 4,
+
+    }
+  },
+})
+
+const checkWidthWindow = () => {
+  if (window.matchMedia("(max-width: 1199px)").matches) {
+    swiperFeatures.enable()
+    swiperStep.enable()
+  } else {
+    swiperFeatures.disable()
+    swiperStep.disable()
+  }
+}
+checkWidthWindow()
+
+window.addEventListener('resize', () => {
+  checkWidthWindow()
 })
