105fc7773db2ee4fe5afde6dc07584ce1af9a36d
polliterru <polinasilnagina3313@gmail.com>
2023-01-14 21:25:32 +0200

    fix: modal button close

--- js/main.js
+++ js/main.js
@@ -117,12 +117,22 @@ const swiperBlog = new Swiper(".blog-slider", {
     },
   },
 });
-const modal = document.querySelector(".modal");
-const modalDialog = document.querySelector(".modal-dialod");
+const modal = document.querySelector('.modal');
+const modalDialog = document.querySelector('.modal-dialog');
 
-document.addEventListener("click", (event) => {
-  console.log(
-    event.target.dataset.toggle == "modal" ||
-      event.target.parentNode.dataset.loggle == "modal"
-  );
+document.addEventListener('click', (event) => {
+  if (
+    event.target.dataset.toggle == 'modal' ||
+    event.target.parentNode.dataset.toggle == 'modal' ||
+    (!event.composedPath().includes(modalDialog) && 
+      modal.classList.contains('is-open'))
+  ) {
+    modal.classList.toggle('is-open');
+  }
+});
+
+document.addEventListener('keyup', (event) => {
+  if (event.key == 'Escape' && modal.classList.contains('is-open')) {
+    modal.classList.toggle('is-open');
+  }
 });
